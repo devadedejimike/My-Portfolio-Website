e4cc634ebc0ca326ba05078af4a836542e80df61
OutsydBaby <amichdot@gmail.com>
2026-03-01 09:58:48 +0100

    feat: Added framer-motion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -6,19 +7,22 @@ import Projects from "./components/Projects";
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 
-
-
 const App = () => {
   return (
-    <>
-      <Header/>
-      <Home/>
-      <About/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+      className="overflow-x-hidden"
+    >
+      <Header />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
       <ScrollToTop />
-    </>
+    </motion.div>
   );
 };
 
